539312f735a3cd31e2291b571e9380e18f6a34a3
Biscuit <105267308+BiscuitVG@users.noreply.github.com>
2026-02-17 13:10:18 +0530

    Create traker.sh
    -setUserInput()
    >Argument validation.
    >Date format.
    >Namerefs for arguments.
    >DB file - Not wokring as intented.

---
+++ tracker.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+declare Habit="$1"
+declare Date="$2"
+
+setUserInput() {
+    local -n userHabit="$1"
+    local -n userDate="$2"
+
+    if [[ -z "$userHabit" ]]
+    then
+        echo "Arguments were not satisfied!"
+        sleep 1
+        exit 1
+    else
+        echo "Arguments are satisfied!"
+        sleep 1
+    fi
+
+    if [[ -z "$userDate" ]]
+    then
+        echo "Arguments were not satisfied!"
+        sleep 1
+        exit 1
+    else
+        if ! [[ "$userDate" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]
+        then
+            echo "Arguments were not satisfied!: Expected date in YYYY-MM-DD format."
+            exit 1
+        else
+            echo "Arguments are satisfied!"
+            sleep 1
+        fi
+    fi
+
+    DBFile="DBStoreUser.DB"
+
+    if [[ -f "$DBFile" ]]
+    then
+        echo "DB Active!"
+    else
+        echo "DB Inactive!"
+        sleep 1
+
+        touch "$DBFile"
+        echo "New DB is Active!"
+    fi
+}
+
+setUserInput Habit Date
