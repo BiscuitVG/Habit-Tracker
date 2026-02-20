0238555e5e23180c427823b40aaf97cda7a4080b
Biscuit <105267308+BiscuitVG@users.noreply.github.com>
2026-02-20 12:02:55 +0530

    Update setDBData
    *echo statements >>> stderr
    > setDBData contains basic logic, reads the file and split using delimiter to vars.

--- tracker.sh
+++ tracker.sh
@@ -9,47 +9,61 @@ setUserInput() {
 
     if [[ -z "$userHabit" ]]
     then
-        echo "Arguments were not satisfied!"
+        echo "Arguments were not satisfied!" >&2
         sleep 1
         exit 1
     else
-        echo "Arguments are satisfied!"
+        echo "Arguments are satisfied!" >&2
         sleep 1
     fi
 
     if [[ -z "$userDate" ]]
     then
-        echo "Arguments were not satisfied!: Date set to current."
+        echo "Arguments were not satisfied!: Date set to current." >&2
         sleep 1
         userDate=$(date '+%Y-%m-%d')
     else
-        if ! [[ "$userDate" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]
+        if ! [[ "$userDate" =~ ^[0-9]{4}/[0-9]{2}/[0-9]{2}$ ]]
         then
-            echo "Arguments were not satisfied!: Expected date in YYYY-MM-DD format."
+            echo "Arguments were not satisfied!: Expected date in YYYY-MM-DD format." >&2
             exit 1
         else
-            echo "Arguments are satisfied!"
+            echo "Arguments are satisfied!" >&2
             sleep 1
         fi
     fi
 
-    DBFile="DBStoreUser.DB"
+    DBFile="DBStoreUserTemp.DB"
 
     if [[ -f "$DBFile" ]]
     then
-        echo "DB Active!"
+        echo "DB Active!" >&2
         sleep 1
     else
-        echo "DB Inactive!"
+        echo "DB Inactive!" >&2
         sleep 1
 
         touch "$DBFile"
-        echo "New DB is Active!"
+        echo "New DB is Active!" >&2
     fi
+}
 
-    echo "Test Var:"
-    echo "Habit: $userHabit"
-    echo "Date: $userDate"
+setDBData() {
+    local -n userHabit="$1"
+    local -n userDate="$2"
+    local ref_DBFile="$3"
+
+    local DB_Habit #temp vars to save file split values
+    local DB_Date
+
+    while IFS='|' read -r DB_Habit DB_Date || [[ -n "$DB_Habit" ]] #reads each line in DBfile and splits using the delimiter to either vars | [ -n] used to ignore the /n rule in IFS
+    do
+        echo "$DB_Habit"
+        echo "$DB_Date"
+        sleep 1
+    done < "$ref_DBFile"
+        
 }
 
 setUserInput Habit Date
+setDBData Habit Date "$DBFile"
